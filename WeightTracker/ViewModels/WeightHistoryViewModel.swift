fe8c8e9c5882a96b46dee50b1576f67fde43d12b
sherary <sherary.apriliana@gmail.com>
2026-08-22 22:57:21 +0700

    [FEAT]: add segment picker for filtering | separating history view with the viewmodel | refactoring query fetching methods

--- WeightTracker/ViewModels/WeightHistoryViewModel.swift
+++ WeightTracker/ViewModels/WeightHistoryViewModel.swift
@@ -1,3 +1,37 @@
+//import Combine
+import Foundation
+
+@Observable
 final class WeightHistoryViewModel {
+    var selectedWeight: Weight?
+    var weightToEdit: Weight?
+    var errorMessage: String?
+    var selectedFilter: DataFilter = .weekly {
+        didSet {
+            calculateDateRange()
+        }
+    }
+    internal private(set) var dateRange: DateInterval = .init(start: .now, duration: 0)
     
+    init() {
+        calculateDateRange()
+    }
+    
+    private func calculateDateRange() {
+        if let dateRange = getDateRange() {
+            setDateRange(dateRange: dateRange)
+        }
+    }
+    
+    private func getDateRange(for date: Date = Date.now) -> DateInterval? {
+        switch selectedFilter {
+        case .weekly: return CalendarService.ISO8601.getWeeklyDateRange(for: date)
+        case .monthly: return CalendarService.ISO8601.getMonthlyDateRange(for: date)
+        case .yearly: return CalendarService.ISO8601.getYearlyDateRange(for: date)
+        }
+    }
+    
+    private func setDateRange(dateRange: DateInterval) {
+        self.dateRange = dateRange
+    }
 }
